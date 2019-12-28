6975f43323e77c1f61196b97f9617e0a668c40cf
Lisa Chandra <lisachandra761@gmail.com>
2019-12-28 15:22:44 +0000

    made port changes to app file

--- app.js
+++ app.js
@@ -58,6 +58,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/", indexRoutes);
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
 	console.log("The YelpCamp Server Has Started!");
 });
